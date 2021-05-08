00623cab6d0656323ea6d9c07a5f90e7dfe7e892
anonymous9125 <48875358+anonymous9125@users.noreply.github.com>
2021-05-08 18:16:51 +0500

    database migrations added with models

---
+++ database/migrations/2021_05_08_125033_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('CustomerName');
+            $table->string('ContactNo');
+            $table->integer('EventID');
+            $table->double('Bill');
+            $table->integer('menuID');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
